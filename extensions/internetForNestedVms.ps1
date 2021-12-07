62cf8a8214bfb1d623aee78c4229d0933f6a85f3
Chris Langford <chris.langford@lang-ford.uk>
2021-12-07 17:55:19 +0000

    added file

---
+++ extensions/internetForNestedVms.ps1
@@ -0,0 +1,17 @@
+#################################################################
+# Script to configure to configure Azure Host Hyper-V interface #
+# Author: Chris Langford                                        #
+# Version: 1.0.0                                                # 
+#################################################################
+
+# This script provides internet access to nested VMs in Azure by giving an IP address to a vSwitch
+# the config is applied to the Azure host and the guest is given  a static IP in the revelant range
+
+# Create an internal virtual switch
+New-VMSwitch -SwitchName "Int-vSwitch" -SwitchType Internal
+
+# Set an IPv4 address to the internal vSwtich to be used as a default gateway
+New-NetIPAddress -InterfaceAlias "vEthernet (Int-vSwitch)" -IPAddress 192.168.1.254 -PrefixLength 24
+
+# Crete a NAT rule for an entire subnet
+New-NetNat -Name Nat_VM -InternalIPInterfaceAddressPrefix 192.168.1.0/24
